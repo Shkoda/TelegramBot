ca1ca2270698faeaeb723f1714ae5346e565fb98
Shkoda <shkoshka@gmail.com>
2016-08-05 00:27:05 +0300

    refactor

--- TelegramBot/Main.fs
+++ TelegramBot/Main.fs
@@ -6,57 +6,40 @@ module Main =
 
   let TOKEN  = "231953668:AAHUQ8HEQr8Scnl_ViDZ6dWtH9JXDeMy5hw"
   let PREFIX = "/"
-  let (>>=) = Telegram.(>>=)
+
   let sleep (x: int) = System.Threading.Thread.Sleep x
 
-  let handleJson (message : JsonParser.Update.Result) =
-
-    let reply text = Telegram.sendMessage TOKEN  message.Message.Chat.Id text
+  let handle (update : JsonParser.Update.Result) =
+    let reply text = Telegram.sendMessage TOKEN  update.Message.Chat.Id text
 
     let match_entity (e: JsonParser.Update.Entity) =
-        let value = message.Message.Text.Substring(e.Offset, e.Length)
+        let sender =  update.Message.From.FirstName
+        let value = update.Message.Text.Substring(e.Offset, e.Length)
         match e.Type with
         |"mention" ->  reply ("you've mentioned "+ value)
         |"bot_command" -> 
             let command = value
             match command with
-             |"/hi"|"/hello" -> reply ("Nice to meet you, " + message.Message.From.FirstName)
-             |_ -> reply (sprintf "I don't know %s command, %s" command message.Message.From.FirstName)
-        |"hashtag" -> reply ("you've used hashtag "+value)
+             |"/hi"|"/hello" -> reply ("Nice to meet you, " + sender)
+             |_ -> reply (sprintf "I don't know %s command, %s" command sender)
+        |"hashtag" -> reply ("you've used hashtag " + value)
         |_ -> ignore()
 
-    let rec match_entities (entities: JsonParser.Update.Entity[]) = 
-        match entities with
-        |[||] -> ()
-        |_ as arr -> match_entity(arr.[0]); match_entities(Array.sub arr 1 (arr.Length-1))
-
-    match_entities message.Message.Entities
+    for e in update.Message.Entities do match_entity(e)
 
   let rec mainLoop (offset: int) =
-    let newUpdate = Telegram.getUpdatesJson0 TOKEN offset
-    let newOffset = Telegram.getNewId  newUpdate
+    let update = Telegram.getUpdates TOKEN offset
+    let newOffset = Telegram.getNewId  update
 
     sleep 100 
 
-    match newUpdate with
-    | Some upd-> Seq.iter (fun res -> handleJson res) upd.Result
+    match update with
+    | Some upd-> Seq.iter (fun res -> handle res) upd.Result
     | None -> ignore()
      
-
-    match newOffset with
-      | Some nO -> mainLoop nO
-      | None    -> mainLoop 0 
+    mainLoop newOffset
 
   [<EntryPoint>]
   let main args =
-
-    let initial = Telegram.getUpdatesJson TOKEN 
-    let nextId = Telegram.getNewId initial
-    match initial with
-    |Some updates ->       
-        match nextId with
-        |Some i -> mainLoop i
-        |None -> mainLoop 0
-    |None -> mainLoop 0
-
+    mainLoop 0
     0
--- TelegramBot/Telegram.fs
+++ TelegramBot/Telegram.fs
@@ -2,59 +2,38 @@
 
 module Telegram =
   open FSharp.Data
-  open FSharp.Data.JsonExtensions
-  open System
 
   let JSONEXN = "An issue was encountered parsing the JSON object."
   let HTTPEXN = "An issue was encountered contacting the server."
 
-  //  (?=) :: JsonValue -> a -> option JsonValue 
-  let (?=) (value: JsonValue) prop =
-    value.TryGetProperty prop
-
   let succ x = x + 1
 
-  let (>>=) a f =
-    Option.bind f a
+  let endpoint token path = "https://api.telegram.org/bot" + token + "/" + path
+  let updateEndpoint token =  endpoint token "getUpdates"
+  let sendMessageEndpoint token =  endpoint token "sendMessage"
 
-  let getEndpoint token path = 
-    "https://api.telegram.org/bot" + token + "/" + path
-
-  let getUpdatesJson token =
+  let getUpdates token offset =
     try 
-      getEndpoint token "getUpdates" |> 
-        Http.RequestString |> JsonParser.Update.Parse |> Some
-    with
-      | :? System.Net.WebException -> printfn "%s (%s)" HTTPEXN __LINE__ ; None
-      | _                          -> printfn "%s (%s)" JSONEXN __LINE__ ; None
-
-  let getUpdatesJson0 token offset=
-    try 
-      let url = getEndpoint token "getUpdates"
-      let soffset = sprintf "%d" offset
-      Http.RequestString (url, query=["offset", soffset]) |> JsonParser.Update.Parse |> Some
+      let url = updateEndpoint token 
+      Http.RequestString (url, query=["offset", offset.ToString()]) |> JsonParser.Update.Parse |> Some
     with
       | :? System.Net.WebException -> printfn "%s (%s)" HTTPEXN __LINE__ ; None
       | _                          -> printfn "%s (%s)" JSONEXN __LINE__ ; None    
-
-
-  let sendMessage (token: string) (chatId: int) (body: string) =
-    let url  = getEndpoint token "sendMessage"
-    let stringChatId = sprintf "%d" chatId
-    try
-      Http.RequestString (url, query=["chat_id", stringChatId; "text", body]) |> ignore 
-    with
-      | :? System.Net.WebException -> printfn "%s (%s)" HTTPEXN __LINE__ |> ignore
-      | _                          -> printfn "%s (%s)" HTTPEXN __LINE__ |> ignore
-
-
+ 
   let getNewId (responce: JsonParser.Update.Root option) = 
        let update_id (result:JsonParser.Update.Result) =  result.UpdateId
        match responce with
        |Some r -> 
             match Seq.isEmpty r.Result with
-            | true -> None
-            | false -> r.Result |> Seq.map update_id |> Seq.last |> succ |> Some
-        |None -> None
+            | true -> 0
+            | false -> r.Result |> Seq.map update_id |> Seq.last |> succ 
+        |None -> 0
         
+  let sendMessage token chatId body =
+    let url  = sendMessageEndpoint token 
+    try
+      Http.RequestString (url, query=["chat_id", chatId.ToString(); "text", body]) |> ignore 
+    with
+      | :? System.Net.WebException -> printfn "%s (%s)" HTTPEXN __LINE__ |> ignore
+      | _                          -> printfn "%s (%s)" HTTPEXN __LINE__ |> ignore
 
