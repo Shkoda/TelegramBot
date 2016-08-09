428f68ab40aaea5f42e516e27be5b071271bb53e
Shkoda <shkoshka@gmail.com>
2016-08-09 22:37:28 +0300

    tips and tricks=)

---
+++ TelegramBot/BitBucket.fs
@@ -0,0 +1,10 @@
+﻿module BitBucket
+
+//https://bitbucket.org/site/oauth2/authorize?client_id=MuzSM5B3T2aw8ZGSq4&response_type=code
+//https://bitbucket.org/site/oauth2/authorize?client_id=MuzSM5B3T2aw8ZGSq4&response_type=token
+
+//https://api.telegram.org/bot231953668:AAHUQ8HEQr8Scnl_ViDZ6dWtH9JXDeMy5hw/getUpdates#access_token=mUEswUVix2VSBpHG3qa1XIgpO-_V5hR7chFnfaglsgo5NvKxhWqW8WeP4BCuvj6x8wPzTrrh3eEh7JJO0rM%3D&scopes=pipeline%3Avariable+webhook+snippet%3Awrite+issue%3Awrite+pullrequest+repository%3Awrite+project+team+account%3Awrite&expires_in=3600&token_type=bearer
+
+//redirects to https://api.telegram.org/bot231953668:AAHUQ8HEQr8Scnl_ViDZ6dWtH9JXDeMy5hw/getUpdates?code=5X5yTK9HLfJV2UkKUY
+
+//replace %3D with = =)
