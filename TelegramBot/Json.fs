08f1f1b9d793bf655743f63103cbb4799996ccce
Shkoda <shkoshka@gmail.com>
2016-08-19 12:21:28 +0300

    active pattern for incoming strings

--- TelegramBot/Json.fs
+++ TelegramBot/Json.fs
@@ -5,3 +5,15 @@ module Json =
     type Update = JsonProvider<"../data/update.json">
     type CommitsResponse = JsonProvider<"../data/commits_sample.json">
     type UserConfig = JsonProvider<"../data/user_config_sample.json">
+    type InlineKeybardResponse = JsonProvider<"../data/inline_keyboard_response.json">
+
+    let (|Message|InlineResponce|UnknownFormat|) (jsonString: string) = 
+        try
+            let update = Update.Parse jsonString 
+            Message (update)        
+        with
+        |_ -> try 
+                let inlineResponse = InlineKeybardResponse.Parse jsonString
+                InlineResponce (inlineResponse)
+                with 
+                | _ -> UnknownFormat (jsonString)
