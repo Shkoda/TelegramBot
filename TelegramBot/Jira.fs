d259592a53e547f5b1b52603701194481cd3b2d9
Shkoda <shkoshka@gmail.com>
2016-08-20 22:22:15 +0300

    jira requests

--- TelegramBot/Jira.fs
+++ TelegramBot/Jira.fs
@@ -3,16 +3,16 @@ module Jira =
     open Option
 
     let userinfo (config:Json.UserConfig.Jira) = 
-        let searchUserEndpoint = sprintf "/rest/api/2/user/search?username=%s" config.Email
+        let searchUserEndpoint = sprintf "/rest/api/2/myself" 
         let url = config.Url + searchUserEndpoint
-        let getUser (data:Json.JiraSearchUser.Root[]) = 
-            match data.Length with
-            |0 -> None
-            |_ -> Some (data.[0])
         let toString (user: Json.JiraSearchUser.Root option) = 
             match user with
             |Some data -> TelegramMarkdown.jiraUserAsString data
-            |None -> sprintf "jira user %s not found" config.Email
+            |None -> sprintf "jira user %s not found for %s" config.Email config.Url
+
         Http.authorizedRequest url config.Email config.Password 
-        |> bind (Json.JiraSearchUser.Parse >> getUser) 
+        |> bind (Json.JiraSearchUser.Parse >> Some) 
         |> toString
+
+        //active sprint https://reddotsquare.atlassian.net/rest/agile/1.0/board/28/sprint?state=active
+        // https://reddotsquare.atlassian.net/rest/agile/1.0/board/28/sprint/274/issue?jql=(assignee=currentuser() OR assignee=null) AND status not in ("To review", "QA Ready", "Done") 
